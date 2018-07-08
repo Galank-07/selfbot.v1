120866e189547522878c248b97b0e7766a04e67a
Galank-07 <31801682+Galank-07@users.noreply.github.com>
2018-07-08 18:54:42 +0800

    Update __init__.py

--- linepy/__init__.py
+++ linepy/__init__.py
@@ -1,13 +1,13 @@
 from .client import LineClient
 from .channel import LineChannel
 from .poll import LinePoll
-from Oswib.ttypes import OpType
+from Galank.ttypes import OpType
 
 __copyright__       = 'Copyright 2017 by  WIB'
 __version__         = '2.0.2'
 __license__         = 'THB'
-__author__          = 'WIB'
-__author_email__    = 'oscwib1@gmail.com'
-__url__             = 'http://github.com/oscwib/wib-py3'
+__author__          = 'SLACKBOT'
+__author_email__    = 'gallank.aja@gmail.com'
+__url__             = 'http://github.com/Galank-07/selfbot.v1'
 
 __all__ = ['LineClient', 'LineChannel', 'LinePoll', 'OpType']
